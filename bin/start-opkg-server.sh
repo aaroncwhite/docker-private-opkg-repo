f0613fa8934233246735118f1b9c89f984f17677
Nicolas Marty <nicolas.marty@zuehlke.com>
2021-09-28 23:38:22 +0200

    build package list periodically

--- bin/start-opkg-server.sh
+++ bin/start-opkg-server.sh
@@ -1,17 +1,25 @@
 #!/bin/sh
 
 # 1. Build package list
+build_package_list()
+{
+	cd /packages
+	rm -rf Packages.gz
+	echo "Building package list"
+	find . -name Packages.gz -exec rm {} +
+	find . -name Packages.stamps -exec rm {} +
 
-cd /packages
-echo "Building package list"
-find . -name Packages.gz -exec rm {} +
-find . -name Packages.stamps -exec rm {} +
+	/usr/bin/opkg-make-index . > Packages
+	gzip ./Packages
 
-/usr/bin/opkg-make-index . > Packages
-gzip ./Packages
-
-chmod -R 755 /packages
-
+	chmod -R 755 /packages
+}
 
 echo "Starting nginx"
-nginx -g "daemon off;"
+nginx -g "daemon off;" &
+
+while [ : ]
+do
+	build_package_list
+	sleep 60
+done
